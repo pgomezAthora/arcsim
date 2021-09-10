a0e88ef4974bd973b348c96c39cae5d40e591e35
pgomezAthora <89336721+pgomezAthora@users.noreply.github.com>
2021-09-10 11:07:10 +0100

    add dashboard and a few functionality

--- src/app/core/components/menu/routes.ts
+++ src/app/core/components/menu/routes.ts
@@ -6,8 +6,8 @@ export interface NavItem {
 
 export const NavRoutes: NavItem[] = [
     { name: 'Dashboard', route: '/', icon: 'home' },
-    { name: 'Reporting', route: '/', icon: 'assignment' },
-    { name: 'SBA', route: '/', icon: 'view_array' },
-    { name: 'Queue', route: '/', icon: 'format_list_numbered' },
-    { name: 'Admin', route: '/', icon: 'account_box' }
+    { name: 'Reporting', route: '/demo-input-table/edit', icon: 'assignment' },
+    { name: 'SBA', route: '/demo-input-table/edit', icon: 'view_array' },
+    { name: 'Queue', route: '/demo-input-table/edit', icon: 'format_list_numbered' },
+    { name: 'Admin', route: '/demo-input-table/edit', icon: 'account_box' }
 ];
--- src/app/core/components/menu/side-bar/side-bar.component.ts
+++ src/app/core/components/menu/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavItem, NavRoutes } from '../routes';
 
 @Component({
@@ -8,4 +9,10 @@ import { NavItem, NavRoutes } from '../routes';
 })
 export class SideBarComponent {
     pages: NavItem[] = NavRoutes;
+
+    constructor(private router: Router) {}
+
+    isActive(route: string): boolean {
+        return this.router.isActive(route, true);
+    }
 }
